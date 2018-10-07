b43c505e52ac5716a75f6ed849fb075393b9c4cf
Vincent L <liberty.sky87@gmail.com>
2018-10-06 18:41:47 -0700

    save

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,47 +1,41 @@
-var fs = require('fs');
-var url = require('url');
+var fs = require("fs");
+var url = require("url");
 var path = require("path");
+var http = require("http");
+var mimeTypes = {
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "js": "text/javascript",
+  "css": "text/css"
+};
 
-var messages = {results:[]};
-
-var staticBasePath = './client';
+var results = require("./results");
+var messagesLog = results;
 
 var requestHandler = function(request, response) {
-
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
   var statusCode = 200;
   var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'application/json';
 
-   if (request.url === '/classes/messages'){
+  var requestPath = url.parse(request.url).pathname;
+  if (requestPath === '/'){
+    response.writeHead(303, {Location: '/client/index.html'});
+    response.end();
+  } else if (requestPath.startsWith('/client')) {
+    var mimeType = mimeTypes[requestPath.split('.').pop()];
+    if (!mimeType) { mimeType = 'text/plain';}
+    response.writeHead(200, { "Content-Type": mimeType });
 
-      //static render *TO-DO*
-      // var resolvedBase = path.resolve(staticBasePath);
-      // var safeSuffix = path.normalize(request.url).replace(/^(\.\.[\/\\])+/, '');
-      // var fileLoc = path.join(resolvedBase, safeSuffix);
-
-      // var stream = fs.createReadStream(fileLoc);
-
-      //   // Handle non-existent file
-      //   stream.on('error', function(error) {
-      //       response.writeHead(404, 'Not Found');
-      //       response.write('404: File Not Found!');
-
-      //   });
-
-      //   // File exists, stream it to user
-      //   response.statusCode = 200;
-      //   stream.pipe(response);
-        //end static render
-
-     if(request.method === 'GET'){
+    fs.readFile('.' + requestPath, (err, data) => {
+      if (err) {console.log(err.message || err); }
+      response.end(data);
+    });
+  } else if (requestPath === '/classes/messages'){
+    if(request.method === 'GET'){
         response.writeHead(200, headers);
-        var data = JSON.stringify(messages);
+        var data = JSON.stringify(messagesLog);
         response.end(data);
-      }
-
-      else if (request.method ==='POST'){
+    } else if (request.method ==='POST'){
         statusCode = 201;
         response.writeHead(statusCode, headers);
 
@@ -49,41 +43,46 @@ var requestHandler = function(request, response) {
           var obj ='';
           obj+= chunk;
           var res = JSON.parse(obj);
+          res.objectId = genID(JSON.stringify(res.username+res.text+res.createdAt));
+          res.roomName = 'lobby';
           res.createdAt = new Date();
-          res.objectId = genID(JSON.stringify(res));
-          if (!exists(res.objectId)){
-            messages.results.push(res);
+          res.updatedAt = res.createdAt;
+
+          var chk = exists(res.objectId);
+          if (chk>-1){
+            console.log('id exists, updating existing msg');
+            res.createdAt = messagesLog.results[chk].createdAt;
+            messagesLog.results.splice(chk,1);
+            res.updatedAt = new Date();
           }
+          messagesLog.results.push(res);
 
         });
-        response.end(JSON.stringify(messages.results));
-      }
-       else if (request.method ==='OPTIONS'){
-       }
-       else if (request.method ==='PUT'){
-       }
-       else if (request.method ==='DELETE'){
-       }
-
-    } else {
-        statusCode=404;
-        response.writeHead(statusCode, headers);
-        response.end();
+        var resArr = JSON.stringify(messagesLog);
+        response.end(resArr);
+    } else if (request.method ==='OPTIONS'){
+    } else if (request.method ==='PUT'){
+    } else if (request.method ==='DELETE'){
     }
+  } else {
+    statusCode=404;
+    response.writeHead(statusCode, headers);
+    response.end();
+  }
 
 }; //end request handler
 
 var exists = (id) => {
-  for (var i =0;i<messages.results.length;i++){
-    if (messages.results[i].objectId === id){
-      return true;
+  for (var i =0;i<messagesLog.results.length;i++){
+    if (messagesLog.results[i].objectId === id){
+      return i;
     }
   }
-  return false;
+  return -1;
 }
 
 var genID = (str) => {
-  var str = str.split('').sort().slice(0,18).join('');
+  var str = str.split('').slice(0,18).join('');
   var hash = 0, i, chr;
   if (str.length === 0) return hash;
   for (i = 0; i < str.length; i++) {
@@ -105,23 +104,12 @@ var genID = (str) => {
 
 };
 
-//url http://127.0.0.1:3000/
-//msg format createdAt: "2018-10-05T22:48:50.106Z"
-/*
-objectId: "eJQikOUSc2"
-roomname: "lobby"
-text: "asdfasdf"
-updatedAt: "2018-10-05T22:48:50.106Z"
-username: "anonymous"
-*/
-
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type':'application/json'
 };
-
-
 
 module.exports.requestHandler = requestHandler;
